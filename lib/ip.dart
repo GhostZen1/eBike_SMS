364d67a039d89ca871b07481f18ef568a9fef7d1
GhostZen1 <112802330+GhostZen1@users.noreply.github.com>
2025-01-11 01:59:37 +0800

    updated code

--- lib/ip.dart
+++ lib/ip.dart
@@ -1,7 +1,7 @@
 import 'package:latlong2/latlong.dart';
 
 class ApiBase {
-static String amir = "http://192.168.0.31/e-bike";
+  static String amir = "http://192.168.0.31/e-bike";
   static String king = "http://192.168.10.155/e-bike";
   static String kingHouse = "http://192.168.1.117/e-bike";
   static String kingUTeM = "http://10.131.75.68/e-bike";
@@ -10,5 +10,5 @@ static String amir = "http://192.168.0.31/e-bike";
   static String hosting = "https://etourmersing.com/Ebike_API/";
 
   // Alternatively, if you just need the base URL as a string
-  static String get baseUrl => iman;
+  static String get baseUrl => hosting;
 }
